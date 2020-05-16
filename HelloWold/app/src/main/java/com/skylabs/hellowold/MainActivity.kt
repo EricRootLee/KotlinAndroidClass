c2b9207daf89b5ba1356e9bee5b69c8c48505027
Eric Muli <erickiokom@gmail.com>
2020-05-16 15:08:22 +0300

    Refactored The code

--- HelloWold/app/src/main/java/com/skylabs/hellowold/MainActivity.kt
+++ HelloWold/app/src/main/java/com/skylabs/hellowold/MainActivity.kt
@@ -5,18 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var text = findViewById<TextView>(R.id.textView)
-        val btn = findViewById<Button>(R.id.button2)
+       // var text = findViewById<TextView>(R.id.textView)
+     //   val btn = findViewById<Button>(R.id.button2)
          var clicked:Int = 0;
-        btn.setOnClickListener {
-            clicked = clicked +1
-            text.text = "You Clicked Me $clicked Times";
+        button2.setOnClickListener {
+          //  clicked = clicked +1
+            clicked +=1
+            textView.text = "You Clicked Me $clicked Times";
             Toast.makeText(this,"You Clicked me" , Toast.LENGTH_LONG).show()
         }
 
