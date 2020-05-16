d4c7af4f5483468fd6b0e8ed643311b407d5f9bf
Eric Muli <erickiokom@gmail.com>
2020-05-16 23:16:44 +0300

    Worked On when

--- HelloWold/app/src/main/java/com/skylabs/hellowold/kotlinbasics.kt
+++ HelloWold/app/src/main/java/com/skylabs/hellowold/kotlinbasics.kt
@@ -1,5 +1,11 @@
 package com.skylabs.hellowold
 
 fun main(){
-
+var seasons = 20
+    when (seasons){
+       in  1..4 -> println("First Quater")
+       in  5..8 -> println("2nd Quater")
+            1,2 -> println("First 2 months")
+           else -> println("Invalid response")
+    }
 }
