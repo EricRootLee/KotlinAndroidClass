5875c4ab30bb48c2e00289943ab7cda2a325d981
Eric Muli <erickiokom@gmail.com>
2020-05-17 03:00:56 +0300

    Worked on init

--- HelloWold/app/src/main/java/com/skylabs/hellowold/classes.kt
+++ HelloWold/app/src/main/java/com/skylabs/hellowold/classes.kt
@@ -1,5 +1,13 @@
 package com.skylabs.hellowold
 
 fun main(){
+    var msee = Person()
+var deno = Person("muli", "eric")
+}
+
+class  Person (fname:String =" ERic" ,lname:String = "Muli"){ //you can create default classes to be called when the class is initialized with out attributes
+    init {
+        println("Person Created $fname")
+    }
 
 }
