0a878808c0a8f6f0772891652519dbf94b6544d7
Eric Muli <erickiokom@gmail.com>
2020-05-17 03:46:51 +0300

    Setters And Getters

--- HelloWold/app/src/main/java/com/skylabs/hellowold/classes.kt
+++ HelloWold/app/src/main/java/com/skylabs/hellowold/classes.kt
@@ -7,6 +7,14 @@ fun main(){
     deno.hobby = "Scatting"
     deno.stateHobby()
     deno.age = 45
+
+    /////////////////class car
+
+    var mycar = Car()
+    mycar.maxspped = 8
+    println(mycar.maxspped)
+
+
 }
 ////////////////////////////////Class Person
 class  Person (fname:String =" ERic" ,lname:String = "Muli"){ //you can create default classes to be called when the class is initialized with out properties
@@ -27,6 +35,26 @@ class  Person (fname:String =" ERic" ,lname:String = "Muli"){ //you can create d
         firstName = fname
         println("Person Created $fname ")
     }
+}
 
+////////////////////////////////////Car
+class  Car(){
+    lateinit var owner:String
+    var myBrand :String = "BMW"
+        get() {
+            return  field.toLowerCase()
+        }
+
+    var maxspped = 200
+        set(value) {
+            field = if(value > 0 ) value else throw IllegalArgumentException("field cant be less than 0")
+        }
+        var myModel : String ="MD5"
+            private  set
+
+    init {
+        this.owner = "Frank"
+        print(owner)
+    }
 
 }
