41f37fbb542f8c0589d4b5b943d88acbd7f35c70
Eric Muli <erickiokom@gmail.com>
2020-05-23 02:20:09 +0300

    App is working

--- AgeApp/app/src/main/java/com/skylabs/ageapp/MainActivity.kt
+++ AgeApp/app/src/main/java/com/skylabs/ageapp/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.time.Month
 import java.time.MonthDay
 import java.time.Year
@@ -29,21 +31,30 @@ class MainActivity : AppCompatActivity() {
         val month = myCalender.get(Calendar.MONTH)
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
+      val dpd =  DatePickerDialog(
             this,
             DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
                 calculateMinutes(selectedYear, selectedMonth + 1, selectedDayOfMonth)
-                // Toast.makeText(this,"Choosen Year  $year", Toast.LENGTH_LONG).show()
 
             },
             year,
             month,
             day
-        ).show()
+        )
+        dpd.datePicker.setMaxDate(Date().time - 84600000)
+        dpd.show()
     }
 
     private fun calculateMinutes(year: Any, month: Any, day: Any) {
-        Toast.makeText(this, "The year is $year , $month , $day", Toast.LENGTH_LONG).show()
+        val selecteddate= "$day/$month/$year"
+        tv.setText(selecteddate)
+        val sdf = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH)
+        val thedate = sdf.parse(selecteddate)
+        val selecteddateinMinutes = thedate!!.time / 60000
+        val currentdate = sdf.parse(sdf.format(System.currentTimeMillis()))
+        val currentdayinmin = currentdate!!.time /60000
+        val minutes = (currentdayinmin - selecteddateinMinutes).toString()
+           ageInMinutes.setText(minutes)
 
     }
 
