85a6e80e1c999a32b2eb9b71d9c392d1576a0ad2
Eric Muli <erickiokom@gmail.com>
2020-05-23 02:25:35 +0300

    Done

--- AgeApp/app/src/main/java/com/skylabs/ageapp/MainActivity.kt
+++ AgeApp/app/src/main/java/com/skylabs/ageapp/MainActivity.kt
@@ -4,13 +4,8 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.time.Month
-import java.time.MonthDay
-import java.time.Year
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,11 +26,10 @@ class MainActivity : AppCompatActivity() {
         val month = myCalender.get(Calendar.MONTH)
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
 
-      val dpd =  DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
             DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
                 calculateMinutes(selectedYear, selectedMonth + 1, selectedDayOfMonth)
-
             },
             year,
             month,
@@ -46,17 +40,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateMinutes(year: Any, month: Any, day: Any) {
-        val selecteddate= "$day/$month/$year"
+        val selecteddate = "$day/$month/$year"
         tv.setText(selecteddate)
-        val sdf = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH)
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
         val thedate = sdf.parse(selecteddate)
         val selecteddateinMinutes = thedate!!.time / 60000
         val currentdate = sdf.parse(sdf.format(System.currentTimeMillis()))
-        val currentdayinmin = currentdate!!.time /60000
+        val currentdayinmin = currentdate!!.time / 60000
         val minutes = (currentdayinmin - selecteddateinMinutes).toString()
-           ageInMinutes.setText(minutes)
+        ageInMinutes.setText(minutes)
 
     }
-
 }
 
