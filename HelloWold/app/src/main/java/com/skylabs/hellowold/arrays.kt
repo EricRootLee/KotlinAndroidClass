6e41535b51a9d8256eb31111c101eab6c03eb17a
Eric Muli <erickiokom@gmail.com>
2020-05-22 03:43:14 +0300

    Started Module 5

--- HelloWold/app/src/main/java/com/skylabs/hellowold/arrays.kt
+++ HelloWold/app/src/main/java/com/skylabs/hellowold/arrays.kt
@@ -1,2 +1,21 @@
 package com.skylabs.hellowold
 
+fun main(){
+
+////empty array
+    val arr = ArrayList<String>()
+    arr.add("Ine")
+    arr.add("dfhsakj0")
+    println("araaaaaat")
+    for (i in arr){
+        println(i)
+    }
+    arr.get(1) //gets  a specific index
+
+    ///arrays using collections
+
+    ///lambda expressions
+
+
+
+}
