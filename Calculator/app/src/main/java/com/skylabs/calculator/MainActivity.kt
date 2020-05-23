d25d735fef6b5385b0ca832bd1f89a24a6df0b7c
Eric Muli <erickiokom@gmail.com>
2020-05-23 23:44:19 +0300

    Working on the layouts

--- Calculator/app/src/main/java/com/skylabs/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/skylabs/calculator/MainActivity.kt
@@ -2,12 +2,22 @@ package com.skylabs.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val d : Double = 2.0000000
+        val text : Double? = Math.floor(d)
+
+    }
+
+    fun onDigit(view:View){
+
+        Toast.makeText(this,"It works",Toast.LENGTH_LONG).show()
     }
 }
