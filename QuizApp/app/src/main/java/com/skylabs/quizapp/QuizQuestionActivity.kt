3be76b13a925281d8c6ed140af3cb1337a099dfa
Eric Muli <erickiokom@gmail.com>
2020-05-24 19:50:53 +0300

    Question Class

--- QuizApp/app/src/main/java/com/skylabs/quizapp/QuizQuestionActivity.kt
+++ QuizApp/app/src/main/java/com/skylabs/quizapp/QuizQuestionActivity.kt
@@ -2,11 +2,15 @@ package com.skylabs.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class QuizQuestionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+
     }
 }
