0e0fc00cc61843b08fb7514912d575e2bf63c854
Eric Muli <erickiokom@gmail.com>
2020-05-24 05:27:00 +0300

    done with the calculator

--- Calculator/app/src/main/java/com/skylabs/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/skylabs/calculator/MainActivity.kt
@@ -78,7 +78,30 @@ class MainActivity : AppCompatActivity() {
                         valuone = prefix + valuone
                     }
                     tvInput.text = (valuone.toDouble() - valuetwo.toDouble()).toString()
-
+                } else if (tvValue.contains("+")) {
+                    val spltvalue = tvValue.split("+")
+                    var valuone = spltvalue[0]
+                    var valuetwo = spltvalue[1]
+                    if (!prefix.isEmpty()) {
+                        valuone = prefix + valuone
+                    }
+                    tvInput.text = (valuone.toDouble() + valuetwo.toDouble()).toString()
+                } else if (tvValue.contains("/")) {
+                    val spltvalue = tvValue.split("/")
+                    var valuone = spltvalue[0]
+                    var valuetwo = spltvalue[1]
+                    if (!prefix.isEmpty()) {
+                        valuone = prefix + valuone
+                    }
+                    tvInput.text = (valuone.toDouble() / valuetwo.toDouble()).toString()
+                } else if (tvValue.contains("*")) {
+                    val spltvalue = tvValue.split("*")
+                    var valuone = spltvalue[0]
+                    var valuetwo = spltvalue[1]
+                    if (!prefix.isEmpty()) {
+                        valuone = prefix + valuone
+                    }
+                    tvInput.text = (valuone.toDouble() * valuetwo.toDouble()).toString()
                 }
 
             } catch (e: ArithmeticException) {
