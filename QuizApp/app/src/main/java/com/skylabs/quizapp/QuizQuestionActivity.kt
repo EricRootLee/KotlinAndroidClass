61b0e20e597ab5d1ea4b6be443b92abfc83c8efd
Eric Muli <erickiokom@gmail.com>
2020-05-27 03:08:59 +0300

    Linked the ui with the logic

--- QuizApp/app/src/main/java/com/skylabs/quizapp/QuizQuestionActivity.kt
+++ QuizApp/app/src/main/java/com/skylabs/quizapp/QuizQuestionActivity.kt
@@ -2,16 +2,35 @@ package com.skylabs.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 class QuizQuestionActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
         val questionList = Constants.getQuestions()
-        Log.i("Questions Acctivity", "Questions Length ${questionList.size}")
-    }
+        val currentPosition = 1
+        val question : Question? = questionList[currentPosition - 1]
+
+        progressbar.progress = currentPosition
+        progressNumber.text = "$currentPosition " + "/" + progressbar.max
+
+        tvQuestion.text = question!!.question
+        iv_image.setImageResource(question.image)
+
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+
+
+     }
+
+
+
 }
